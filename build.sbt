750ee899246640d88123cff27f6dbc54122d6957
codeOverFlow <adrien.bodineau@gmail.com>
2015-11-06 09:51:15 +0100

    initial

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "ri-cross"
+
+version := "1.0"
+
+scalaVersion := "2.11.7"
+    
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,9 @@
+/**
+ * Created by codeoverflow on 06/11/15.
+ */
+object Main {
+  def main(args: Array[String]) {
+    // TODO: all
+
+  }
+}
