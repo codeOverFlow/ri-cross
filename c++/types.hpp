37b549d3afa1a01825c9487d098767ab4e91cfee
codeOverFlow <adrien.bodineau@gmail.com>
2015-11-13 11:29:41 +0100

    add some new types

--- c++/types.hpp
+++ c++/types.hpp
@@ -3,6 +3,13 @@
 #include <string>
 #include <map>
 #include <set>
+#include <utility>
 
 typedef std::set<std::string> appax_set;
 typedef std::map<std::string, appax_set> appax_file;
+typedef appax_file::iterator ite_appax_f;
+
+typedef std::map<std::string, int> appar_score;
+typedef std::map<std::string, appar_score> appariement_map;
+
+typedef std::map<int, std::set<std::pair<std::string, std::string>>> inverse_t;
